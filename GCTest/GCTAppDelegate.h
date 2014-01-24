d3b65cec5f828c0155216e3973f39f0e6393c92b
Roberto Scarciello <rscarciello@gmail.com>
2014-01-24 03:11:59 +0100

    Initial Commit

---
+++ GCTest/GCTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GCTAppDelegate.h
+//  GCTest
+//
+//  Created by Roberto Scarciello on 24/01/14.
+//  Copyright (c) 2014 Roberto Scarciello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GCTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GCTest
+//
+//  Created by Roberto Scarciello on 24/01/14.
+//  Copyright (c) 2014 Roberto Scarciello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GCTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GCTAppDelegate class]));
+    }
+}
